d12761b7d7afcf7894acb89870d7e3ef2fb649d0
Gonzalo Martinez <150065620+GonzaloMartinezz@users.noreply.github.com>
2025-05-19 12:51:46 -0300

    creando Navbar

--- CoinsApp/src/App.jsx
+++ CoinsApp/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
- import './App.css'
+import './App.css'
+import Navbar from './components/Navbar'
 import HomeScreen from './pages/HomeScreen'
 import Login from './pages/Login'
 import ErrorScreen from './pages/ErrorScreen'
 import AdminScreen from './pages/AdminScreen'
 import AboutScreen from './pages/AboutScreen'
 
+
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/login" element={<Login />} />
