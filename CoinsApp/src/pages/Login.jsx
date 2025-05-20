e92f4e5f82e81377bef4e1ac8a6093abff590af1
Gonzalo Martinez <150065620+GonzaloMartinezz@users.noreply.github.com>
2025-05-20 15:46:58 -0300

    validando campos del logIn

--- CoinsApp/src/pages/Login.jsx
+++ CoinsApp/src/pages/Login.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate();
     //estados para cada uno de los campos//
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleChange = (e) => {
       setEmail(e.target.value);
+    }
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+   
+
+      const user = {
+        email: "gonza@gonza.com",
+        password: "123456",
+
+      }
+
+      if(!email || !password){
+        setShowAlert(true);
+      }
+
+      //validaciones//
+      if(email === user.email && password === user.password){
+        navigate("/");
+      }else{
+        setShowAlert(true);
+      }
     }
     return (
         <>
@@ -19,7 +44,7 @@ const Login = () => {
                 </div>
                 <div className="row">
                     <div className="col col-md-6 offset-md-3">
-                        <form onsubmit="">
+                        <form onsubmit={handleSubmit}>
                             <div className="mb-3 d-grid">
                                 <label>Email </label>
                                 <input 
@@ -27,7 +52,8 @@ const Login = () => {
                                 className="form-control" 
                                 name='email'
                                 value={email} 
-                                onChange={handleChange}/>
+                                onChange={handleChange}                      
+                                />
                             </div>
                             <div className="mb-3 d-grid">
                                 <label>Password</label>
@@ -40,6 +66,8 @@ const Login = () => {
                                     setPassword(e.target.value)
                                 }/>
                             </div>
+                            {showAlert && <p className='bg-danger text-light text-center m-3'>Faltan datos,complete todos los campos</p>}
+                           
                             <div className='mb-3 d-grid'>
                                 <button type="submit" className="btn btn-success">Sign In</button>
                             </div>
